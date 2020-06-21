90fbdc9c9df278eccd30507fcbee560055e02b3e
DESKTOP-03G7PES\felid <felid.codas@gmail.com>
2020-06-21 10:04:51 +0900

    UIヘッダ制作中。。。

--- work_SNAProj/UI.h
+++ work_SNAProj/UI.h
@@ -1,9 +1,16 @@
 #pragma once
 #include <SDL/SDL.h>
 
+class Shader;
+
 class UI
 {
 public:
 	UI();
-	~UI();
+	virtual ~UI();
+
+	virtual void Update();
+	virtual void Draw(Shader * shader);
+	virtual void ProcessInput(const uint8_t * keys);		//“ü—Í‚ğó‚¯æ‚éˆ—
+	virtual void HandleKeyPress(int key);
 };
