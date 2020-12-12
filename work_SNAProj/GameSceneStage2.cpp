fdc1e27257609dfe71f62d9cea0912e8e310cd3a
DESKTOP-03G7PES\felid <felid.codas@gmail.com>
2020-12-12 22:34:02 +0900

    2つ目のステージのゲームシーンクラスを作成するだけ作成。

---
+++ work_SNAProj/GameSceneStage2.cpp
@@ -0,0 +1,10 @@
+#include "GameSceneStage2.h"
+
+GameSceneStage2::GameSceneStage2():
+	GameScene("Map/Map1/map.csv", "SM_Ice_RuinedWalls.png", "Assets / SM_Snow_Rock_Wall_A.png")
+{
+}
+
+GameSceneStage2::~GameSceneStage2()
+{
+}
---
+++ work_SNAProj/GameSceneStage2.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "GameScene.h"
+
+class GameSceneStage2 : public GameScene
+{
+public:
+	GameSceneStage2();
+	~GameSceneStage2();
+};
