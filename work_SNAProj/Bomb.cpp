1aa59ec8bea8169826f4c3038f5dbd9fe4f89000
null <null>
2021-02-09 16:40:57 +0900

    爆発範囲のコリジョンを定義。

--- work_SNAProj/Bomb.cpp
+++ work_SNAProj/Bomb.cpp
@@ -1,10 +1,26 @@
 #include "Bomb.h"
-
+#include "BoxColliderComponent.h"
+#include "MeshComponent.h"
+#include "Mesh.h"
 Bomb::Bomb():
 	Block("Assets/Bomb.png", false)
 {
+	// 爆発時、自身のコリジョン何個分の爆発範囲が生じるか
+	float explosionRangeRate = 4.0f;
+
+	AABB box = mMeshComponent->GetMesh()->GetCollisionBox();
+	box.mMin *= explosionRangeRate / 2.0f;
+	box.mMax *= explosionRangeRate / 2.0f;
+
+	mExplosionCollider = new BoxColliderComponent(this, ColliderAttribute::ColAtt_Detector);
+	mExplosionCollider->SetActive(false);
 }
 
 Bomb::~Bomb()
 {
+}
+
+void Bomb::OnHit(const ColliderComponentBase * caller, const ColliderComponentBase * opponent)
+{
+
 }
--- work_SNAProj/Bomb.h
+++ work_SNAProj/Bomb.h
@@ -1,9 +1,14 @@
 #pragma once
 #include "Block.h"
+
 class Bomb : public Block
 {
 public:
 	Bomb();
 	~Bomb();
-};
 
+	void OnHit(const ColliderComponentBase * caller, const ColliderComponentBase * opponent) override;
+
+private:
+	BoxColliderComponent * mExplosionCollider;
+};
